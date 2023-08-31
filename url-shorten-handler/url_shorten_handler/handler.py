5ac0087015ab461f877cc58814400af5ce29e620
Bartosz Perczynski <bartosz_p@hotmail.com>
2023-08-31 14:43:54 +0100

    feat: refactored handler

--- url-shorten-handler/url_shorten_handler/handler.py
+++ url-shorten-handler/url_shorten_handler/handler.py
@@ -1,40 +1,24 @@
-import json
-import os
-
-import boto3
-import shortuuid
-
-from url_shorten_handler.logic import post_url, get_url, get_url_stats, delete_url
-from url_shorten_handler.model.shortened_url import ShortenedUrl
+from url_shorten_handler.logic import (
+    handle_post_url,
+    handle_get_url,
+    handle_get_url_stats,
+    handle_delete_url,
+)
 from url_shorten_handler.utils import get_route_path
 
 
 def lambda_handler(event, context):
-    print(event)
-    route, path = get_route_path(event)
-    if route == "POST" and path == "/url":
-        return post_url(event, context)
-    elif route == "GET" and path == "/url/{proxy+}" or path == "/{proxy+}":
-        return get_url(event, context)
-    elif route == "DELETE" and path == "/url/{proxy+}":
-        return delete_url(event, context)
-    elif route == "GET" and path == "/url/stats/{proxy+}":
-        return get_url_stats()
-    else:
-        return f"Not implemented -> {event['routeKey']}"
+    print(f"Event: {event}")
+    handlers = {
+        ("POST", "/url"): handle_post_url,
+        ("GET", "/{proxy+}"): handle_get_url,
+        ("GET", "/url/{proxy+}"): handle_get_url,
+        ("DELETE", "/url/{proxy+}"): handle_delete_url,
+        ("GET", "/url/stats/{proxy+}"): handle_get_url_stats,
+    }
+    handler = handlers.get(get_route_path(event), handle_not_implemented)
+    return handler(event)
 
 
-def old_code(event, context):
-    print(event)
-
-    body = json.loads(event["body"])
-    print(body)
-    short_url = ShortenedUrl(id=shortuuid.uuid(), original_url=body["url"])
-
-    dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table(os.environ["SHORTEN_URLS_TABLE"])
-    table.put_item(Item=json.loads(short_url.model_dump_json()))
-
-    # fixme unsupported Type URL
-
-    return {"statusCode": 200, "body": short_url.model_dump_json()}
+def handle_not_implemented(event):
+    return f"Not implemented -> {event['routeKey']}"
--- url-shorten-handler/url_shorten_handler/logic.py
+++ url-shorten-handler/url_shorten_handler/logic.py
@@ -3,25 +3,41 @@ import os
 
 import boto3
 import shortuuid
+from pydantic import ValidationError
 
 from url_shorten_handler.model.shortened_url import ShortenedUrl
 from url_shorten_handler.utils import get_proxy_param
 
 
-def post_url(event, context):
-    body = json.loads(event["body"])
-    short_url = ShortenedUrl(
-        id=shortuuid.uuid(),
-        original_url=body["url"],
-    )
-
-    print(f"Shortened URL: {short_url.model_dump_json()}")
+def handle_post_url(event):
+    request_body = json.loads(event["body"])
+    try:
+        short_url = ShortenedUrl(
+            id=shortuuid.uuid(),
+            original_url=request_body["url"],
+        )
+        print(f"Shortened URL: {short_url.model_dump_json()}")
+    except KeyError:
+        print(
+            f"Failed to create shortened, due to url parameter missing. {request_body}"
+        )
+        return {
+            "statusCode": 400,
+            "body": "Body missing 'url' parameter",
+        }
+    except ValidationError:
+        print(f"Failed to create shortened URL for request: {request_body}")
+        return {
+            "statusCode": 400,
+            "body": f"Invalid URL: {request_body['url']}",
+        }
 
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(os.environ["SHORTEN_URLS_TABLE"])
     table.put_item(
         Item=json.loads(short_url.model_dump_json()),
     )
+
     print(f"Saved to DynamoDB: {short_url.model_dump_json()}")
     return {
         "statusCode": 200,
@@ -29,14 +45,13 @@ def post_url(event, context):
     }
 
 
-def get_url(event, context):
+def handle_get_url(event):
     short_url_id = get_proxy_param(event)
     print(f"id: {short_url_id}")
 
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(os.environ["SHORTEN_URLS_TABLE"])
     short_url_item = table.get_item(Key={"id": short_url_id})
-    print(f"Item: {short_url_item}")
 
     if "Item" not in short_url_item:
         print(f"No Items found with id: {short_url_id}")
@@ -44,6 +59,15 @@ def get_url(event, context):
             "statusCode": 404,
             "body": "Not found",
         }
+
+    # todo when retrieving short_url,
+    #  todo append the IP address
+    #   (storing ip addresses probs isnt that bad but will need to do batch ip-address call)
+    #              OR
+    #  todo call ip-api and get country
+    #   (api-call limits / will it slow down url retrival? or send it to another lambda to process)
+    #              AND
+    #   then append to object.
 
     print(f"Returning URL: {short_url_item['Item']['original_url']}")
     return {
@@ -54,7 +78,7 @@ def get_url(event, context):
     }
 
 
-def delete_url(event, context):
+def handle_delete_url(event):
     short_url_id = get_proxy_param(event)
     print(f"id: {short_url_id}")
 
@@ -67,5 +91,20 @@ def delete_url(event, context):
     }
 
 
-def get_url_stats():
-    return "Logic for GET url stats"
+# todo handle aws pydantic lists that are supported in dyanmodb
+# todo importing and exporting of model into and out of dynamodb
+# call ip api http://ip-api.com/json/{ip}
+# save location to object
+
+
+def handle_get_url_stats(event):
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(os.environ["SHORTEN_URLS_TABLE"])
+    short_url_item = table.get_item(Key={"id": get_proxy_param(event)})["Item"]
+
+    print(f"Item: {json.dumps(short_url_item)}")
+
+    obj = ShortenedUrl(**short_url_item)
+    print(obj)
+
+    return {"statusCode": 200, "body": json.dumps(short_url_item)}
