01d5039682efa2d7a6021a4555325c9116b0ae92
Bartosz Perczynski <bartosz_p@hotmail.com>
2023-10-01 20:48:37 +0100

    feat: changed model to include ip addresses and country click stats

--- url-shorten-handler/url_shorten_handler/model/shortened_url.py
+++ url-shorten-handler/url_shorten_handler/model/shortened_url.py
@@ -1,6 +1,10 @@
+from typing import Optional
+
 from pydantic import BaseModel, AnyHttpUrl
 
 
 class ShortenedUrl(BaseModel):
     id: str
     original_url: AnyHttpUrl
+    addresses: Optional[list] = []
+    country_stats: Optional[dict] = {}
