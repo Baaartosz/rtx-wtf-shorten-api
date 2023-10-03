4f8ba8edc9670b6003756f3667616ffe1ace4ae7
Bartosz Perczynski <bartosz_p@hotmail.com>
2023-10-03 18:59:26 +0100

    feat: added logging

--- url-shorten-handler/url_shorten_handler/logic.py
+++ url-shorten-handler/url_shorten_handler/logic.py
@@ -4,10 +4,14 @@ import os
 import boto3
 import httpx
 import shortuuid
+import logging
 from pydantic import ValidationError
 
 from url_shorten_handler.model.shortened_url import ShortenedUrl
 from url_shorten_handler.utils import get_proxy_param
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.environ["SHORTEN_URLS_TABLE"])
 
 
 def handle_post_url(event):
@@ -17,29 +21,25 @@ def handle_post_url(event):
             id=shortuuid.uuid(),
             original_url=request_body["url"],
         )
-        print(f"Shortened URL: {short_url.model_dump_json()}")
+        logging.info(f"Shortened URL: {short_url.model_dump_json()}")
     except KeyError:
-        print(
-            f"Failed to create shortened, due to url parameter missing. {request_body}"
-        )
+        logging.error(f"Failed to create shortened, due to url parameter missing. {request_body}")
         return {
             "statusCode": 400,
             "body": "Body missing 'url' parameter",
         }
     except ValidationError:
-        print(f"Failed to create shortened URL for request: {request_body}")
+        logging.error(f"Failed to create shortened URL for request: {request_body}")
         return {
             "statusCode": 400,
             "body": f"Invalid URL: {request_body['url']}",
         }
 
-    dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table(os.environ["SHORTEN_URLS_TABLE"])
     table.put_item(
         Item=json.loads(short_url.model_dump_json()),
     )
 
-    print(f"Saved to DynamoDB: {short_url.model_dump_json()}")
+    logging.info(f"Saved to DynamoDB: {short_url.model_dump_json()}")
     return {
         "statusCode": 200,
         "body": short_url.model_dump_json(),
@@ -48,33 +48,35 @@ def handle_post_url(event):
 
 def handle_get_url(event: dict):
     short_url_id = get_proxy_param(event)
-    print(f"id: {short_url_id}")
+    logging.info(f"Received '{short_url_id}' id")
 
-    dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table(os.environ["SHORTEN_URLS_TABLE"])
     short_url_item = table.get_item(Key={"id": short_url_id})
 
     if "Item" not in short_url_item:
-        print(f"No Items found wih id: {short_url_id}")
+        logging.info(f"No Items found wih '{short_url_id}' id")
         return {
             "statusCode": 404,
             "body": "Not found",
         }
 
     # todo optimise same ip address by changing to dictionary and incrementing same addresses
-    source_ip = event.get("requestContext").get("http").get("sourceIp")
-    table.update_item(
-        Key={"id": short_url_id},
-        UpdateExpression="SET #addresses = list_append(if_not_exists(#addresses, :empty_list), :new_address)",
-        ExpressionAttributeNames={"#addresses": "addresses"},
-        ExpressionAttributeValues={
-            ":new_address": [source_ip],
-            ":empty_list": [],
-        },
-    )
-    print(f"Appended '{source_ip}' to url")
+    try:
+        source_ip = event.get("requestContext").get("http").get("sourceIp")
+        logging.info(f"Updating '{short_url_id}' record with '{source_ip}' source ip")
+        table.update_item(
+            Key={"id": short_url_id},
+            UpdateExpression="SET #addresses = list_append(if_not_exists(#addresses, :empty_list), :new_address)",
+            ExpressionAttributeNames={"#addresses": "addresses"},
+            ExpressionAttributeValues={
+                ":new_address": [source_ip],
+                ":empty_list": [],
+            },
+        )
+        logging.info(f"Successfully appended '{source_ip}' to '{short_url_id}'")
+    except Exception as ex:
+        logging.error(f"Exception occurred during source_ip append, {ex}")
 
-    print(f"Returning URL: {short_url_item['Item']['original_url']}")
+    logging.info(f"Returning URL: {short_url_item['Item']['original_url']}")
     return {
         "statusCode": 308,
         "headers": {
@@ -85,12 +87,9 @@ def handle_get_url(event: dict):
 
 def handle_delete_url(event):
     short_url_id = get_proxy_param(event)
-    print(f"id: {short_url_id}")
-
-    dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table(os.environ["SHORTEN_URLS_TABLE"])
+    logging.info(f"Received '{short_url_id}' id")
     short_url_item = table.delete_item(Key={"id": short_url_id})
-    print(f"Item: {short_url_item}")
+    logging.info(f"Deleting '{short_url_id}': {short_url_item}")
     return {
         "statusCode": 204,
     }
@@ -98,14 +97,14 @@ def handle_delete_url(event):
 
 def handle_get_url_stats(event):
     short_url_id = get_proxy_param(event)
-    dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table(os.environ["SHORTEN_URLS_TABLE"])
-    short_url_item = table.get_item(Key={"id": get_proxy_param(event)})["Item"]
+    logging.info(f"Received '{short_url_id}' id")
 
+    short_url_item = table.get_item(Key={"id": get_proxy_param(event)})["Item"]
     obj = ShortenedUrl(**short_url_item)
-    print(f"Short URL: {obj.model_dump_json()}")
+    logging.info(f"Loaded '{short_url_id}' into short url model: {obj.model_dump_json()}")
 
     if not obj.addresses:
+        logging.info(f"Object has {len(obj.addresses)} ip addresses")
         response_list = []
         chunks = [obj.addresses[i : i + 100] for i in range(0, len(obj.addresses), 100)]
         for chunk in chunks:
@@ -120,40 +119,40 @@ def handle_get_url_stats(event):
                 url="http://ip-api.com/batch",
                 json=formatted_batch_ips,
             )
+            logging.debug(f"Sending {formatted_batch_ips} to ip api")
             response_list.append(response.json())
 
         aggregated_data = {
             **obj.country_stats,
-            **{
-                k: v for k, v in obj.country_stats.items() if k not in obj.country_stats
-            },
+            **{k: v for k, v in obj.country_stats.items() if k not in obj.country_stats},
         }
 
-        print(response_list)
-        # Iterate over each entry in the country_stats list
         for entry in response_list[0]:
             country = entry["country"]
-
-            # If the country is not yet in the aggregated_data dictionary, initialize it
             if country not in aggregated_data:
                 aggregated_data[country] = {
                     "clicks": 0,
                 }
-
-            # Increment the total count for the country
             aggregated_data[country]["clicks"] += 1
 
         obj.addresses = []
         obj.country_stats = aggregated_data
 
-        table.update_item(
-            Key={"id": short_url_id},
-            UpdateExpression="REMOVE #addresses SET #country_stats = :country_stats_value",
-            ExpressionAttributeNames={
-                "#addresses": "addresses",
-                "#country_stats": "country_stats",
-            },
-            ExpressionAttributeValues={":country_stats_value": obj.country_stats},
-        )
+        try:
+            table.update_item(
+                Key={"id": short_url_id},
+                UpdateExpression="REMOVE #addresses SET #country_stats = :country_stats_value",
+                ExpressionAttributeNames={
+                    "#addresses": "addresses",
+                    "#country_stats": "country_stats",
+                },
+                ExpressionAttributeValues={":country_stats_value": obj.country_stats},
+            )
+        except Exception as ex:
+            logging.error(f"Failed to update record '{short_url_id}' with new data, {ex}")
 
-    return {"statusCode": 200, "body": obj.model_dump_json()}
+    logging.info(f"Final short url object: {obj.model_dump_json()}")
+    return {
+        "statusCode": 200,
+        "body": obj.model_dump_json(),
+    }
