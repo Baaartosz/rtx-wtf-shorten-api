fec61b271ceba5e57c03c62baca35818bf25fe0d
Bartosz Perczynski <bartosz_p@hotmail.com>
2023-10-21 16:59:08 +0100

    feat: added url controller class

---
+++ url-shorten-handler/url_shorten_handler/controller.py
@@ -0,0 +1,25 @@
+from url_shorten_handler.logic import (
+    handle_post_url,
+    handle_get_url,
+    handle_get_url_stats,
+    handle_delete_url,
+)
+from url_shorten_handler.util.event_util import get_route_path
+
+
+class UrlShortenerController:
+    _ROUTE_MAP = {
+        ("POST", "/url"): handle_post_url,
+        ("GET", "/{proxy+}"): handle_get_url,
+        ("GET", "/url/{proxy+}"): handle_get_url,
+        ("DELETE", "/url/{proxy+}"): handle_delete_url,
+        ("GET", "/url/stats/{proxy+}"): handle_get_url_stats,
+    }
+
+    @classmethod
+    def route_request(cls, event):
+        return cls._ROUTE_MAP.get(get_route_path(event), cls._handle_not_implemented)
+
+    @staticmethod
+    def _handle_not_implemented(event):
+        return f"Not implemented -> {event['routeKey']}"
--- url-shorten-handler/url_shorten_handler/handler.py
+++ url-shorten-handler/url_shorten_handler/handler.py
@@ -1,24 +1,7 @@
-from url_shorten_handler.logic import (
-    handle_post_url,
-    handle_get_url,
-    handle_get_url_stats,
-    handle_delete_url,
-)
-from url_shorten_handler.util.event_util import get_route_path
+from url_shorten_handler.controller import UrlShortenerController
 
 
 def lambda_handler(event, context):
     print(f"Event: {event}")
-    handlers = {
-        ("POST", "/url"): handle_post_url,
-        ("GET", "/{proxy+}"): handle_get_url,
-        ("GET", "/url/{proxy+}"): handle_get_url,
-        ("DELETE", "/url/{proxy+}"): handle_delete_url,
-        ("GET", "/url/stats/{proxy+}"): handle_get_url_stats,
-    }
-    handler = handlers.get(get_route_path(event), handle_not_implemented)
+    handler = UrlShortenerController.route_request(event)
     return handler(event)
-
-
-def handle_not_implemented(event):
-    return f"Not implemented -> {event['routeKey']}"
